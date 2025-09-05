36e6abb3f9d5b9f8f7062acc0fdc110a69e6cb05
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-09-05 15:29:13 -0300

    use sync to access the stub to avoid race conditions

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -97,34 +97,33 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func startLoading() {
-            if let requestObserver = URLProtocolStub.requestObserver {
-                client?.urlProtocolDidFinishLoading(self)
-                return requestObserver(request)
-            }
+            guard let stub = URLProtocolStub.stub else { return }
 
-            if let data = URLProtocolStub.stub?.data {
+            if let data = stub.data {
                 client?.urlProtocol(self, didLoad: data)
             }
 
-            if let response = URLProtocolStub.stub?.response {
+            if let response = stub.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
 
-            if let error = URLProtocolStub.stub?.error {
+            if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
+            } else {
+                client?.urlProtocolDidFinishLoading(self)
             }
 
-            client?.urlProtocolDidFinishLoading(self)
+            stub.requestObserver?(request)
         }
 
         override func stopLoading() {}
 
         static func stub(data: Data?, response: URLResponse?, error: Error?) {
-            stub = Stub(data: data, response: response, error: error)
+            stub = Stub(data: data, response: response, error: error, requestObserver: nil)
         }
 
         static func observeRequests(observer: @escaping (URLRequest) -> Void) {
-            requestObserver = observer
+            stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
 
         static func startInterceptingRequests() {
@@ -143,10 +142,17 @@ class URLSessionHTTPClientTests: XCTestCase {
             let data: Data?
             let response: URLResponse?
             let error: Error?
+            let requestObserver: ((URLRequest) -> Void)?
         }
 
-        private static var stub: Stub?
+        private static var _stub: Stub?
+        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
         private static var requestObserver: ((URLRequest) -> Void)?
+
+        private static var stub: Stub? {
+            get { return queue.sync { _stub } }
+            set { queue.sync { _stub = newValue } }
+        }
 
     }
 
