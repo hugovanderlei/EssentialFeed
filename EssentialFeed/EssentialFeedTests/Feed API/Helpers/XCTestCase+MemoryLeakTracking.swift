d772b35c3f3a1537f6e449c19bd147c2643b28c1
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-09-08 16:41:17 -0300

    update code with essential repo

---
+++ EssentialFeed/EssentialFeedTests/Feed API/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+	func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        if #available(macOS 10.15, *) {
+            addTeardownBlock { [weak instance] in
+                XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+	}
+}
