8278273678e4aa36cbe67aa6d7afe91fb1f4b492
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-09-25 12:51:21 -0300

    Add RemoteFeedItem data transfer representation to decouple the items mapper from FeedItem data models

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -4,34 +4,37 @@
 
 import Foundation
 
-internal final class FeedItemsMapper {
-	private struct Root: Decodable {
-		let items: [Item]
-		
-		var feed: [FeedItem] {
-			return items.map { $0.item }
-		}
-	}
-	
-	private struct Item: Decodable {
-		let id: UUID
-		let description: String?
-		let location: String?
-		let image: URL
-		
-		var item: FeedItem {
-			return FeedItem(id: id, description: description, location: location, imageURL: image)
-		}
-	}
-	
-	private static var OK_200: Int { return 200 }
-	
-	internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-		guard response.statusCode == OK_200,
-			let root = try? JSONDecoder().decode(Root.self, from: data) else {
-			return .failure(RemoteFeedLoader.Error.invalidData)
-		}
+// MARK: - Item
 
-		return .success(root.feed)
-	}
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal  let image: URL
+}
+
+// MARK: - FeedItemsMapper
+
+internal final class FeedItemsMapper {
+
+    // MARK: Internal
+
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+
+        return root.items
+    }
+
+    // MARK: Private
+
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+
+    }
+
+    private static var OK_200: Int { return 200 }
+
 }
--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -4,32 +4,62 @@
 
 import Foundation
 
+// MARK: - RemoteFeedLoader
+
 public final class RemoteFeedLoader: FeedLoader {
-	private let url: URL
-	private let client: HTTPClient
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-	
-	public typealias Result = LoadFeedResult
-	
-	public init(url: URL, client: HTTPClient) {
-		self.url = url
-		self.client = client
-	}
-	
-	public func load(completion: @escaping (Result) -> Void) {
-		client.get(from: url) { [weak self] result in
-			guard self != nil else { return }
-			
-			switch result {
-			case let .success(data, response):
-				completion(FeedItemsMapper.map(data, from: response))
-			case .failure:
-				completion(.failure(Error.connectivity))
-			}
-		}
-	}
+
+    // MARK: Lifecycle
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    // MARK: Public
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public typealias Result = LoadFeedResult
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            switch result {
+            case let .success(data, response):
+                completion(RemoteFeedLoader.map(data, response: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+
+    // MARK: Private
+
+    private let url: URL
+    private let client: HTTPClient
+
+    private static func map(_ data: Data, response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+
+}
+
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        return map { FeedItem(
+            id: $0.id,
+            description: $0.description,
+            location: $0.location,
+            imageURL: $0.image
+        ) }
+    }
 }
