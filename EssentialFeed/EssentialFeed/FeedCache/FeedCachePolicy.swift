47102599309d8faa2dbe94d386c40f5ea899300b
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-09-30 12:03:06 -0300

    Move 'FeedCachePolicy' to its own file

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -0,0 +1,40 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Hugo Vanderlei on 30/09/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedCachePolicy {
+
+    // MARK: Lifecycle
+
+    private init() {}
+
+    // MARK: Internal
+
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard
+            let maxCacheAge = calendar.date(
+                byAdding: .day,
+                value: maxCacheAgeInDays,
+                to: timestamp
+            )
+        else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+
+    // MARK: Private
+
+    private static let calendar = Calendar(identifier: .gregorian)
+
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+
+}
--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -9,34 +9,6 @@ import Foundation
 
 // MARK: - FeedCachePolicy
 
-private final class FeedCachePolicy {
-
-    // MARK: Internal
-    
-    private init() {}
-
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard
-            let maxCacheAge = calendar.date(
-                byAdding: .day,
-                value: maxCacheAgeInDays,
-                to: timestamp
-            )
-        else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-
-    // MARK: Private
-
-    private static let calendar = Calendar(identifier: .gregorian)
-
-    private static var maxCacheAgeInDays: Int {
-        return 7
-    }
-
-}
 
 // MARK: - LocalFeedLoader
 
