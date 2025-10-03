42c0f3a69aa30a69a2755f66fd1e74ddacacc13e
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-10-03 11:03:53 -0300

    Extract FailableRetrieveFeedStoreSpecs checks into reusable assertions

---
+++ EssentialFeed/EssentialFeedTests/FeedCache/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
@@ -0,0 +1,20 @@
+//
+//  XCTestCase+FailableRetrieveFeedStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Hugo Vanderlei on 03/10/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+extension FailableRetrieveFeedStoreSpecs where Self: XCTestCase {
+    func assertThatRetrieveDeliversFailureOnRetrievalError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieve: .failure(anyNSError()), file: file, line: line)
+    }
+
+    func assertThatRetrieveHasNoSideEffectsOnFailure(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieveTwice: .failure(anyNSError()), file: file, line: line)
+    }
+}
