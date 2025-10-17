ee3218cf28fd317077dde58906d33755ffb9503e
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-10-17 12:09:11 -0300

    Make EssentialFeedCacheIntegrationTests target support macOS and iOS since it's platform-independent (can run on any platform!)

---
+++ EssentialFeed/EssentialFeediOS/EssentialFeediOS.swift
@@ -0,0 +1,10 @@
+//
+//  EssentialFeediOS.swift
+//  EssentialFeediOS
+//
+//  Created by Hugo Vanderlei on 17/10/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
