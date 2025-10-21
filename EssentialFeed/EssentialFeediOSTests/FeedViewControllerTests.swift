a6661334c31d6202d10d8aca4ca9524f3af3e8f6
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-10-21 10:50:49 -0300

    Replace FeedViewControllerTests.LoaderSpy references in the production types with the production `FeedLoader` protocol abstraction

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -2,50 +2,70 @@
 //  Copyright © 2019 Essential Developer. All rights reserved.
 //
 
-import XCTest
+import EssentialFeed
 import UIKit
+import XCTest
+
+// MARK: - FeedViewController
 
 final class FeedViewController: UIViewController {
-    private var loader: FeedViewControllerTests.LoaderSpy?
-    
-    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+
+    // MARK: Properties
+
+    private var loader: FeedLoader?
+
+    // MARK: Lifecycle
+
+    convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loader?.load()
+
+        loader?.load { _ in }
     }
 }
 
+// MARK: - FeedViewControllerTests
+
 final class FeedViewControllerTests: XCTestCase {
-    
+
+    // MARK: Nested Types
+
+    // MARK: - Helpers
+
+    class LoaderSpy: FeedLoader {
+
+        // MARK: Properties
+
+        private(set) var loadCallCount: Int = 0
+
+        // MARK: Functions
+
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            loadCallCount += 1
+        }
+
+    }
+
+    // MARK: Functions
+
     func test_init_doesNotLoadFeed() {
         let loader = LoaderSpy()
         _ = FeedViewController(loader: loader)
-        
+
         XCTAssertEqual(loader.loadCallCount, 0)
     }
-    
+
     func test_viewDidLoad_loadsFeed() {
         let loader = LoaderSpy()
         let sut = FeedViewController(loader: loader)
-        
+
         sut.loadViewIfNeeded()
-        
+
         XCTAssertEqual(loader.loadCallCount, 1)
-    }
-    
-    // MARK: - Helpers
-    
-    class LoaderSpy {
-        private(set) var loadCallCount: Int = 0
-        
-        func load() {
-            loadCallCount += 1
-        }
     }
 
 }
