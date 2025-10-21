d316603a7b3895fc503afba3eb3000c073541faf
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-10-21 10:46:19 -0300

    Load feed on view did load

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,47 +1,51 @@
 //
-//  FeedViewControllerTests.swift
-//  EssentialFeediOSTests
-//
-//  Created by Hugo Vanderlei on 17/10/25.
-//  Copyright © 2025 Essential Developer. All rights reserved.
+//  Copyright © 2019 Essential Developer. All rights reserved.
 //
 
 import XCTest
+import UIKit
 
-// MARK: - FeedViewController
-
-final class FeedViewController {
-
-    // MARK: Properties
-
-    var loader: FeedViewControllerTests.LoaderSpy
-
-    // MARK: Lifecycle
-
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+final class FeedViewController: UIViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy?
+    
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
         self.loader = loader
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load()
+    }
 }
 
-// MARK: - FeedViewControllerTests
-
 final class FeedViewControllerTests: XCTestCase {
-
-    // MARK: Nested Types
-
-    class LoaderSpy {
-
-        private(set) var loaderCallCount: Int = 0
-    }
-
-    // MARK: Functions
-
+    
     func test_init_doesNotLoadFeed() {
         let loader = LoaderSpy()
-        let _ = FeedViewController(loader: loader)
-
-        XCTAssertEqual(loader.loaderCallCount, 0)
+        _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 
 }
