9d515e8afdfeb7b7af7a90d661edf01705a05b62
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-10-31 11:46:15 -0300

    Retry failed image load on retry action

--- EssentialFeed/EssentialFeediOS/FeedImageCell.swift
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 public final class FeedImageCell: UITableViewCell {
+
+    // MARK: Properties
+
     public let locationContainer = UIView()
     public let locationLabel = UILabel()
     public let descriptionLabel = UILabel()
     public let feedImageContainer = UIView()
     public let feedImageView = UIImageView()
-    public let feedImageRetryButton = UIButton()
+
+    public private(set) lazy var feedImageRetryButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    var onRetry: (() -> Void)?
+
+    // MARK: Functions
+
+    @objc private func retryButtonTapped() {
+        onRetry?()
+    }
 }
