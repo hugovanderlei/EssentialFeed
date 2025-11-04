0fa6704c6c6b4d422f7725f417abf5f7721606ac
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-11-04 16:15:50 -0300

    Group files in an MVC folder structure

---
+++ EssentialFeed/EssentialFeediOS/Controllers/FeedViewController.swift
@@ -0,0 +1,135 @@
+//
+//  FeedImageDataLoaderTask.swift
+//  EssentialFeed
+//
+//  Created by Hugo Vanderlei on 04/11/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+import UIKit
+
+// MARK: - FeedImageDataLoaderTask
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+// MARK: - FeedImageDataLoader
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
+
+// MARK: - FeedViewController
+
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
+
+    // MARK: Properties
+
+    private var viewAppeared = false
+    private var tableModel = [FeedImage]()
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
+
+    private var feedLoader: FeedLoader?
+    private var imageLoader: FeedImageDataLoader?
+
+    // MARK: Lifecycle
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        tableView.prefetchDataSource = self
+        load()
+    }
+
+    convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
+        self.init()
+        self.feedLoader = feedLoader
+        self.imageLoader = imageLoader
+    }
+
+    // MARK: Overridden Functions
+
+    override public func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+
+        if !viewAppeared {
+            refresh()
+            viewAppeared = true
+        }
+    }
+
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageView.image = nil
+        cell.feedImageContainer.startShimmering()
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+
+            self.tasks[indexPath] = self.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
+
+        cell.onRetry = loadImage
+        loadImage()
+        return cell
+    }
+
+    override public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cancelTask(forRowAt: indexPath)
+    }
+
+    // MARK: Functions
+
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            let cellModel = tableModel[indexPath.row]
+            tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach(cancelTask)
+    }
+
+    func refresh() {
+        refreshControl?.beginRefreshing()
+    }
+
+    private func cancelTask(forRowAt indexPath: IndexPath) {
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
+    }
+
+    @objc private func load() {
+        refresh()
+        feedLoader?.load { [weak self] result in
+            guard let self else { return }
+            if let feed = try? result.get() {
+                self.tableModel = feed
+                self.tableView.reloadData()
+            }
+            self.refreshControl?.endRefreshing()
+        }
+    }
+
+}
