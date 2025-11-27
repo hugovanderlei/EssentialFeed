116bc31a8e01a323f92293c2b1f30158d0cce33f
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-11-27 16:37:01 -0300

    Move MainQueueDispatchDecorator to separate file

--- EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -39,54 +39,6 @@ public final class FeedUIComposer {
     }
 }
 
-// MARK: - MainQueueDispatchDecorator
-
-private final class MainQueueDispatchDecorator<T> {
-
-    // MARK: Properties
-
-    private let decoratee: T
-
-    // MARK: Lifecycle
-
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-
-    // MARK: Functions
-
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-
-        completion()
-    }
-
-}
-
-// MARK: FeedLoader
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-
-}
-
-// MARK: FeedImageDataLoader
-
-extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
 private extension FeedViewController {
     static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composer/MainQueueDispatchDecorator.swift
@@ -0,0 +1,55 @@
+//
+//  MainQueueDecorator.swift
+//  EssentialFeediOS
+//
+//  Created by Hugo Vanderlei on 27/11/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+import Foundation
+
+// MARK: - MainQueueDispatchDecorator
+
+final class MainQueueDispatchDecorator<T> {
+
+    // MARK: Properties
+
+    private let decoratee: T
+
+    // MARK: Lifecycle
+
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+
+    // MARK: Functions
+
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+
+        completion()
+    }
+}
+
+// MARK: FeedLoader
+
+extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+// MARK: FeedImageDataLoader
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
