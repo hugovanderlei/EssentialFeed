0d482f9c00ac3bc9c750c25845d2e4d660beae81
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-10 14:29:50 -0300

    Move FeedViewModel to separate file

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -8,11 +8,6 @@
 
 import Foundation
 
-// MARK: - FeedViewModel
-
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
 
 // MARK: - FeedView
 
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Hugo Vanderlei on 10/12/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
