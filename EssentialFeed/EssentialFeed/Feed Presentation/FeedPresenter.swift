ea4078b393bd1911669e3ebc895ab91c7bef8193
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-10 14:28:13 -0300

    Move prod types to the EssentialFeed production module

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,119 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeed
+//
+//  Created by Hugo Vanderlei on 10/12/25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - FeedViewModel
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
+
+// MARK: - FeedView
+
+public protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
+
+// MARK: - FeedLoadingViewModel
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
+
+// MARK: - FeedLoadingView
+
+public protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
+// MARK: - FeedErrorViewModel
+
+public struct FeedErrorViewModel {
+
+    // MARK: Static Computed Properties
+
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+
+    // MARK: Properties
+
+    public let message: String?
+
+    // MARK: Static Functions
+
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+
+}
+
+// MARK: - FeedErrorView
+
+public protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+// MARK: - FeedPresenter
+
+public final class FeedPresenter {
+
+    // MARK: Static Computed Properties
+
+    public static var title: String {
+        return NSLocalizedString(
+            "FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view"
+        )
+    }
+
+    // MARK: Properties
+
+    private let errorView: FeedErrorView
+    private let loadingView: FeedLoadingView
+    private let feedView: FeedView
+
+    // MARK: Computed Properties
+
+    private var feedLoadError: String {
+        return NSLocalizedString(
+            "FEED_VIEW_CONNECTION_ERROR",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Error message displayed when we can't load the image feed from the server"
+        )
+    }
+
+    // MARK: Lifecycle
+
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.errorView = errorView
+        self.loadingView = loadingView
+        self.feedView = feedView
+    }
+
+    // MARK: Functions
+
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(.error(message: feedLoadError))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
