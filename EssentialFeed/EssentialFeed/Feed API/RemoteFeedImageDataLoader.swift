e2b56fb07362d661a827bc49ad1a29067abf64c6
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-11 16:22:25 -0300

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers connectivity error on HTTP client error

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -6,60 +6,76 @@
 //  Copyright © 2025 Essential Developer. All rights reserved.
 //
 
-
-
 import Foundation
 
 public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
-	private let client: HTTPClient
 
-	public init(client: HTTPClient) {
-		self.client = client
-	}
+    // MARK: Nested Types
 
-	public enum Error: Swift.Error {
-		case invalidData
-	}
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
 
-	private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
-		private var completion: ((FeedImageDataLoader.Result) -> Void)?
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
 
-		var wrapped: HTTPClientTask?
+        // MARK: Properties
 
-		init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-			self.completion = completion
-		}
+        var wrapped: HTTPClientTask?
 
-		func complete(with result: FeedImageDataLoader.Result) {
-			completion?(result)
-		}
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
 
-		func cancel() {
-			preventFurtherCompletions()
-			wrapped?.cancel()
-		}
+        // MARK: Lifecycle
 
-		private func preventFurtherCompletions() {
-			completion = nil
-		}
-	}
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
 
-	@discardableResult
-	public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-		let task = HTTPClientTaskWrapper(completion)
-		task.wrapped = client.get(from: url) { [weak self] result in
-			guard self != nil else { return }
+        // MARK: Functions
 
-			switch result {
-			case let .success((data, response)):
-				if response.statusCode == 200, !data.isEmpty {
-					task.complete(with: .success(data))
-				} else {
-					task.complete(with: .failure(Error.invalidData))
-				}
-			case let .failure(error): task.complete(with: .failure(error))
-			}
-		}
-		return task
-	}
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+
+        func cancel() {
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+
+    // MARK: Properties
+
+    private let client: HTTPClient
+
+    // MARK: Lifecycle
+
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+
+    // MARK: Functions
+
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    task.complete(with: .success(data))
+                } else {
+                    task.complete(with: .failure(Error.invalidData))
+                }
+            case .failure:
+                task.complete(with: .failure(Error.connectivity))
+            }
+        }
+        return task
+    }
 }
