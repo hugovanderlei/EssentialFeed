f35f2c4bdbb4cd2127539e628a3a2e610770ed88
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-15 14:55:59 -0300

    `LocalFeedImageDataLoader.saveImageDataFromURL` does not deliver result after instance has been deallocated

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
@@ -27,7 +27,8 @@ public extension LocalFeedImageDataLoader {
     typealias SaveResult = Result<Void, Error>
 
     func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-        store.insert(data, for: url) { result in
+        store.insert(data, for: url) { [weak self] result in
+            guard self != nil else { return }
             completion(result.mapError { _ in SaveError.failed })
         }
     }
