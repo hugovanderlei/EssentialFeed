1960d00d97da4ff80c37120032719876796ff6fa
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-15 14:53:34 -0300

    `LocalFeedImageDataLoader.saveImageDataFromURL` succeeds on successful store insertion

--- EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
@@ -28,7 +28,7 @@ public extension LocalFeedImageDataLoader {
 
     func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
         store.insert(data, for: url) { result in
-            completion(.failure(SaveError.failed))
+            completion(result.mapError { _ in SaveError.failed })
         }
     }
 }
