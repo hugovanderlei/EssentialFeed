45ad9fe8a2890ff9dd1c7100ec5c7f81d922ebbd
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-16 11:10:02 -0300

    Move `CoreDataFeedStore` implementation of the `FeedStore` protocol to separate file

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -11,7 +11,7 @@ import Foundation
 
 // MARK: - CoreDataFeedStore
 
-public final class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore {
 
     // MARK: Properties
 
@@ -25,41 +25,6 @@ public final class CoreDataFeedStore: FeedStore {
         context = container.newBackgroundContext()
     }
 
-    // MARK: Functions
-
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache.find(in: context).map {
-                    CachedFeed(
-                        feed: $0.localFeed,
-                        timestamp: $0.timestamp
-                    )
-                }
-
-            })
-        }
-    }
-
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
-            completion(Result {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                try context.save()
-            })
-        }
-    }
-
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
-
-            })
-        }
-    }
 
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
