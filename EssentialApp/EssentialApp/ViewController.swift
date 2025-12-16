a82c15718d63f09171e77f10ace855dd6bef4aae
Hugo Vanderlei <hugocvcosta@gmail.com>
2025-12-16 12:23:27 -0300

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Hugo Vanderlei on 16/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
