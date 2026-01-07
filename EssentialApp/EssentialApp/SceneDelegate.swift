5848d290771e996250684a08f2b7089b0a85b71f
Hugo Vanderlei <hugocvcosta@gmail.com>
2026-01-07 14:11:28 -0300

    Add #if DEBUG compilation directive to prevent test-specific code from being deployed in production

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -10,6 +10,8 @@ import EssentialFeed
 import EssentialFeediOS
 import UIKit
 
+// MARK: - SceneDelegate
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     // MARK: Properties
@@ -20,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
-        
+
         let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
 
         let remoteClient = makeRemoteClient()
@@ -30,10 +32,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localStoreURL = NSPersistentContainer
             .defaultDirectoryURL()
             .appendingPathComponent("feed-store.sqlite")
-        
-        if CommandLine.arguments.contains("-reset") {
-            try? FileManager.default.removeItem(at: localStoreURL)
-        }
+        #if DEBUG
+            if CommandLine.arguments.contains("-reset") {
+                try? FileManager.default.removeItem(at: localStoreURL)
+            }
+        #endif
 
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
@@ -58,24 +61,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     private func makeRemoteClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
-            return AlwaysFailingHTTPClient()
+        #if DEBUG
+            if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
+                return AlwaysFailingHTTPClient()
+            }
+        #endif
 
-        default:
-            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }
+
+}
+
+// MARK: - AlwaysFailingHTTPClient
+
+#if DEBUG
+    private class AlwaysFailingHTTPClient: HTTPClient {
+
+        // MARK: Nested Types
+
+        private class Task: HTTPClientTask {
+            func cancel() {}
+        }
+
+        // MARK: Functions
+
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+            completion(.failure(NSError(domain: "offline", code: 0)))
+            return Task()
         }
     }
-
-}
-
-private class AlwaysFailingHTTPClient: HTTPClient {
-    private class Task: HTTPClientTask {
-        func cancel() {}
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        completion(.failure(NSError(domain: "offline", code: 0)))
-        return Task()
-    }
-}
+#endif
