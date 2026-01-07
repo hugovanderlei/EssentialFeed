4aae7afe0e77100da97259fef09c1bbd0bc57f8b
Hugo Vanderlei <hugocvcosta@gmail.com>
2026-01-07 11:52:25 -0300

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -19,7 +19,45 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // MARK: Functions
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-     
+        guard let _ = (scene as? UIWindowScene) else { return }
+
+        let remoteUrl = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+
+        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteUrl, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+
+        window?.rootViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageLoader
+        )
+
+//        let localStoreURL = NSPersistentContainer
+//            .defaultDirectoryURL()
+//            .appendingPathComponent("feed-store.sqlite")
+//
+//        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+//        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+//
+//        let feedViewController = FeedUIComposer.feedComposedWith(
+//            feedLoader: FeedLoaderWithFallbackComposite(
+//                primary: FeedLoaderCacheDecorator(
+//                    decoratee: remoteFeedLoader,
+//                    cache: localFeedLoader
+//                ),
+//                fallback: localFeedLoader
+//            ),
+//            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+//                primary: localImageLoader,
+//                fallback: FeedImageDataLoaderCacheDecorator(
+//                    decoratee: remoteImageLoader,
+//                    cache: localImageLoader
+//                )
+//            )
+//        )
+
+//        window?.rootViewController = feedViewController
     }
 
 }
